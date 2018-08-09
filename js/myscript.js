d7acb28eb80556ea70e8969c2b33beea52540dc5
Nemolite <g25092011@mail.ru>
2018-08-09 14:29:32 +0300

    summer-winter

--- js/myscript.js
+++ js/myscript.js
@@ -7,3 +7,21 @@ $(function(){
     slideWidth: 1200
   });
 });
+
+// summer-winter
+
+
+$(document).ready(function(){
+ 
+   $("#summer").click(function(){
+      $(".chk-summer").css("display","block");
+      $(".chk-winter").css("display","none");
+   });
+
+   $("#winter").click(function(){
+      $(".chk-summer").css("display","none");
+      $(".chk-winter").css("display","block");
+   });
+  
+
+}); 
